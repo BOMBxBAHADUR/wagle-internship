10b2050494c75026b28403de14f0dc8912e882dd
bombxbahadur <katwalutsab36@gmail.com>
2024-09-26 22:54:48 +0545

    using if else statements to find sum for difference of input numbers

--- Dotnet_intern/Program.cs
+++ Dotnet_intern/Program.cs
@@ -4,29 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Friend 1 asks for Friend 2's name
-        Console.Write("Friend 1: Hi! What's your name? ");
-        string friend2Name = Console.ReadLine();
+        Console.Write("x:");
+        var x = int.Parse(Console.ReadLine());
+        Console.Write("y:");
+        var y = int.Parse(Console.ReadLine());
+        Console.Write("operation:");
+        var operation  = (Console.ReadLine());
 
-        // Friend 1 reacts to the name
-        Console.WriteLine($"Friend 1: Wow, {friend2Name} is a nice name!");
-
-        // Friend 1 asks to be friends
-        Console.WriteLine("Friend 1: Will you be my friend?");
-        string friend2Response = Console.ReadLine();
-
-        // Respond to Friend 2's answer
-        if (friend2Response.ToLower() == "yes")
+        if (operation == "+")
         {
-            Console.WriteLine("Friend 1: Awesome! We’re friends now!");
+            Console.WriteLine(x+y);
         }
         else
         {
-            Console.WriteLine("Friend 1: That's okay, maybe another time.");
-        }
-
-        // Ending the conversation
-        Console.WriteLine("Friend 1: It was nice talking to you!");
+            Console.WriteLine( x- y);
+        }    
     }
     
 }
