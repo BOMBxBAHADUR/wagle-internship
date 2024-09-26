b07acfb0428f3bc8d6831566ff1289d266d16d5c
bombxbahadur <katwalutsab36@gmail.com>
2024-09-26 21:46:54 +0545

    creating a small conversation like app with between two friends.

--- Dotnet_intern/Program.cs
+++ Dotnet_intern/Program.cs
@@ -4,15 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter the first number:");
-        int number1 = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine("Enter the second number:");
-        int number2 = Convert.ToInt32(Console.ReadLine());
-        
-        int sum = number1 + number2;
-        
-        Console.WriteLine("The sum of {0} and {1} is: {2}", number1, number2, sum);
+        // Friend 1 asks for Friend 2's name
+        Console.Write("Friend 1: Hi! What's your name? ");
+        string friend2Name = Console.ReadLine();
+
+        // Friend 1 reacts to the name
+        Console.WriteLine($"Friend 1: Wow, {friend2Name} is a nice name!");
+
+        // Friend 1 asks to be friends
+        Console.WriteLine("Friend 1: Will you be my friend?");
+        string friend2Response = Console.ReadLine();
+
+        // Respond to Friend 2's answer
+        if (friend2Response.ToLower() == "yes")
+        {
+            Console.WriteLine("Friend 1: Awesome! We’re friends now!");
+        }
+        else
+        {
+            Console.WriteLine("Friend 1: That's okay, maybe another time.");
+        }
+
+        // Ending the conversation
+        Console.WriteLine("Friend 1: It was nice talking to you!");
     }
     
 }
